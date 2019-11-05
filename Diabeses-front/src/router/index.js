8acad4705562b5abffdb95b248926af3aa618e61
GavsLima <48319774+GavsLima@users.noreply.github.com>
2019-11-04 21:27:27 -0300

    Criação da tela cadastro de usuário

--- Diabeses-front/src/router/index.js
+++ Diabeses-front/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import TelaInicial from '../views/TelaInicial.vue'
+import CadastroUsuario from '../views/CadastroUsuario.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'TelaInicial',
       component: TelaInicial
+    },
+    {
+      path: '/cadastro-usuario',
+      name: 'CadastroUsuario',
+      component: CadastroUsuario
     }
   ]
 })
---
+++ Diabeses-front/src/views/CadastroUsuario.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <div id="Campos">
+          Nome: 
+          <b-form-input v-model="this.NomeUsuario" placeholder="Enter your name"></b-form-input>
+            <input type="radio" id="Homem" value="Masculino" v-model="this.Sexo">
+            <input type="radio" id="Mulher" value="Feminino" v-model="this.Sexo">
+      </div>
+  </div>
+</template>
+
+<script>
+import config from "../helpers/generalConfig";
+
+export default {
+  data() {
+    return {
+        NomeUsuario: "",
+        Sexo: "",
+    };
+  },
+
+  methods: {
+
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
